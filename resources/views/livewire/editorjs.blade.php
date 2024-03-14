8fe6c4d36cf4fc013ecc061547ca6bbfff57d994
Kieran McClure <kieranmcclure97@gmail.com>
2024-03-14 17:25:05 +0000

    Trying to fix focousout

--- resources/views/livewire/editorjs.blade.php
+++ resources/views/livewire/editorjs.blade.php
@@ -1,10 +1,3 @@
-<div
-    x-data="editorInstance('data', '{{ $editorId }}', {{ $readOnly ? 'true' : 'false' }}, '{{ $placeholder }}', '{{ $logLevel }}')"
-    x-init="initEditor()"
-    class="{{ $class }}"
-    style="{{ $style }}"
-    wire:ignore
-    wire:key='{{ $editorId }}'
->
+<div x-data="editorInstance('data', '{{ $editorId }}', {{ $readOnly ? 'true' : 'false' }}, '{{ $placeholder }}', '{{ $logLevel }}')" x-init="initEditor()" class="{{ $class }}" style="{{ $style }}" wire:ignore>
     <div id="{{ $editorId }}"></div>
 </div>
