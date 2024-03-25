b25ac98577ad0d90490a165648d14db1ca7a173c
Kieran McClure <kieranmcclure97@gmail.com>
2024-03-25 12:34:19 +0000

    Added refreshContent method

--- resources/views/livewire/editorjs.blade.php
+++ resources/views/livewire/editorjs.blade.php
@@ -1,4 +1,3 @@
-<div x-data="editorInstance('data', '{{ $editorId }}', {{ $readOnly ? 'true' : 'false' }}, '{{ $placeholder }}', '{{ $logLevel }}')" x-init="initEditor()" @refresh-content.window="updateContent($event.detail.content)"
-    class="{{ $class }}" style="{{ $style }}" wire:ignore>
+<div x-data="editorInstance('data', '{{ $editorId }}', {{ $readOnly ? 'true' : 'false' }}, '{{ $placeholder }}', '{{ $logLevel }}')" x-init="initEditor()" class="{{ $class }}" style="{{ $style }}" wire:ignore>
     <div id="{{ $editorId }}"></div>
 </div>
